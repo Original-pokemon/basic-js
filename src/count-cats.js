3f1c12d39aeb757d11e3cffac23105c0ffced927
original-pokemon <eremkin-02@mail.ru>
2021-03-25 15:21:16 +0300

    feat: add countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let catCount = [];
+
+  for (let elem of matrix) {
+    let arr = elem.filter((e) => {
+      return e === "^^";
+    });
+
+    catCount = catCount.concat(arr);
+  }
+
+  return catCount.length;
 };
