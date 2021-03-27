c001b04885d6a5756d48ef082a8cab8f47a84727
original-pokemon <eremkin-02@mail.ru>
2021-03-27 22:53:18 +0300

    feat: add what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+
+  try {
+    date.getTime();
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  const month = date.getMonth();
+
+  if (1 < month && month < 5) {
+    return "spring";
+  }
+  if (4 < month && month < 8) {
+    return "summer";
+  }
+  if (7 < month && month < 11) {
+    return "autumn";
+  }
+  if (0 <= month || month === 11) {
+    return "winter";
+  }
 };
