fdc227d2a7f8a7f1f64ccad58c850970d9a7ea98
Original-pokemon <eremkin-02@mail.ru>
2021-03-29 14:46:32 +0300

    feat: add tansform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(err);
+  }
+
+  const obj = {
+    "--double-next": 1,
+    "--double-prev": 2,
+    "--discard-next": 3,
+    "--discard-prev": 4,
+  };
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    if (obj[elem]) {
+      switch (obj[elem]) {
+        case 1:
+          if (i < arr.length - 1) {
+            result.push(arr[i + 1]);
+          }
+          break;
+        case 2:
+          if (i > 0) {
+            result.push(result[result.length - 1]);
+          }
+          break;
+        case 3:
+          if (i < arr.length) {
+            result.push(undefined);
+            i += 1;
+          }
+          break;
+        case 4:
+          if (i > 0) {
+            result.pop();
+          }
+          break;
+      }
+    } else {
+      result.push(elem);
+    }
+  }
+
+  const res = result.filter(function (x) {
+    return x !== undefined;
+  });
+
+  return res;
 };
