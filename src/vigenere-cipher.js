775643e6e95ad0858ab082fef37ebec676752ec6
Original-pokemon <eremkin-02@mail.ru>
2021-03-29 22:05:09 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type;
+  }
+  encrypt(message, key) {
+    message = message.toUpperCase();
+    let keyStr = key.toUpperCase();
+
+    const messageLength = message.length;
+    keyStr = keyStr.repeat(Math.ceil(messageLength / key.length)).split("");
+
+    let res = "";
+    for (let i = 0; i < messageLength; i += 1) {
+      if (65 <= message.charCodeAt(i) && message.charCodeAt(i) < 91) {
+        res += String.fromCharCode(
+          ((message.charCodeAt(i) + keyStr[0].charCodeAt(0)) % 26) + 65
+        );
+        keyStr.shift();
+      } else {
+        res += message.charAt(i);
+      }
+    }
+
+    if (this.type === false) {
+      return res.split("").reverse().join("");
+    }
+    return res;
+  }
+  decrypt(message, key) {
+    message = message.toUpperCase();
+    let keyStr = key.toUpperCase();
+
+    const messageLength = message.length;
+    keyStr = keyStr.repeat(Math.ceil(messageLength / key.length)).split("");
+
+    let res = "";
+    for (let i = 0; i < messageLength; i += 1) {
+      if (65 <= message.charCodeAt(i) && message.charCodeAt(i) < 91) {
+        res += String.fromCharCode(
+          ((message.charCodeAt(i) + 26 - keyStr[0].charCodeAt(0)) % 26) + 65
+        );
+        keyStr.shift();
+      } else {
+        res += message.charAt(i);
+      }
+    }
+
+    if (this.type === false) {
+      return res.split("").reverse().join("");
+    }
+    return res;
   }
 }
 
