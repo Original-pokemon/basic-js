43e9b478a296de1cd74ff0e97484ce8090fccc16
Original-pokemon <eremkin-02@mail.ru>
2021-03-29 21:23:17 +0300

    feat: add exrended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  limb = {
+    createAdditionArr(addition, additionRepeatTimes) {
+      let additionArr = [];
+
+      if (!additionRepeatTimes) {
+        additionRepeatTimes = 1;
+      }
+
+      if (typeof addition != "string") {
+        addition = String(addition);
+      }
+
+      if (addition === "undefined") {
+        addition = "";
+      }
+
+      for (let i = 0; i < additionRepeatTimes; i++) {
+        additionArr.push(addition);
+      }
+
+      return additionArr;
+    },
+    joinAdditionArr(additionArr, additionSeparator) {
+      if (!additionSeparator) {
+        additionSeparator = "|";
+      }
+
+      return additionArr.join(additionSeparator);
+    },
+    createStrArr(str, repeatTimes, addition) {
+      let strArr = [];
+
+      if (!repeatTimes) {
+        repeatTimes = 1;
+      }
+
+      for (let i = 0; i < repeatTimes; i++) {
+        strArr.push(`${str}${addition}`);
+      }
+
+      return strArr;
+    },
+    joinStrArr(strArr, separator) {
+      if (!separator) {
+        separator = "+";
+      }
+      return strArr.join(separator);
+    },
+  };
+
+  const additionArr = limb.createAdditionArr(
+    options.addition,
+    options.additionRepeatTimes
+  );
+  const addition = limb.joinAdditionArr(additionArr, options.additionSeparator);
+  const strArr = limb.createStrArr(str, options.repeatTimes, addition);
+  const result = limb.joinStrArr(strArr, options.separator);
+
+  return result;
 };
-  
